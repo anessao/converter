926452c5f8ebd250a54baeaa99af608c8c89daaa
Anessa Ortner <anessa.ortner@gmail.com>
2017-02-08 09:10:35 -0600

    figured out radio checked and cleaned up variables

--- converter.js
+++ converter.js
@@ -1,3 +1,11 @@
+//Variables declaring references
+var answer = document.getElementById("answer");
+var toFarRadio = document.getElementById("toFar");
+var toCelRadio = document.getElementById("toCel");
+var button = document.getElementById("calculate");
+var clearButton = document.getElementById("clear");
+
+//Calculation functions
 function toCelsius (degree) {
 	var celsius = ((degree - 32) * 5) / 9;
 	return celsius;
@@ -7,31 +15,18 @@ function toFahrenheit (degree) {
 	var fahrenheit = ((degree * 9) / 5) + 32;
 	return fahrenheit;
 }
-var toFRadio = document.getElementById("toFar");
-var toCRadio = document.getElementById("toCel");
 
-// Get a reference to the button element in the DOM
-var button = document.getElementById("calculate");
 
-// This function should determine which conversion should
-// happen based on which radio button is selected.
-function determineConverter (clickEvent) {
-  console.log("event", clickEvent);
-}
-if (toFRadio.checked === true){
-	console.log("To Fahrenheit!")
-}
-
-// // Assign a function to be executed when the button is clicked
-// button.addEventListener("click", determineConverter);
-
-var calcButton = document.getElementById("calculate")
-calcButton.addEventListener("click", function(){
-    var x = document.getElementById("original").value;
-    var answer = document.getElementById("answer");
-    if (determineConverter() === true) {
-    answer.innerHTML = toCelsius(x);
-	}	
+button.addEventListener("click", function(){
+    if (toCelRadio.checked) {
+	var degree = document.getElementById("original").value;
+    console.log(degree);
+    answer.innerHTML = toCelsius(degree);
+	} else if (toFarRadio.checked){
+	var degree = document.getElementById("original").value;
+    console.log(degree);
+    answer.innerHTML = toFahrenheit(degree);
+	}
 });
 
 
